710213b08dcce1d2ddec28ffec32b26d5aa565cb
Omar Yarleque <omaryar7.12@gmail.com>
2019-07-24 17:18:16 -0500

    envio de script
    envio de script 24/07/2019

--- app/Models/Parametro.php
+++ app/Models/Parametro.php
@@ -10,3 +10,14 @@ class Parametro extends Model
     protected $fillable = ['codigo','codtab',
     'descor','deslar','valent','valdec'] ;
 }
+/*
+INSERT INTO `afocat`.`parametros` (`codigo`, `descor`, `deslar`, `valent`) VALUES ('1', 'Ciudades', 'Ciudades', '0');
+INSERT INTO `afocat`.`parametros` (`codigo`, `codtab`, `descor`, `deslar`, `valent`) VALUES ('1', '01', 'Piura', 'Piura', '1');
+INSERT INTO `afocat`.`parametros` (`codigo`, `codtab`, `descor`, `deslar`, `valent`) VALUES ('1', '02', 'Ayabaca', 'Ayabaca', '2');
+INSERT INTO `afocat`.`parametros` (`codigo`, `codtab`, `descor`, `deslar`, `valent`) VALUES ('1', '03', 'Huancabamba', 'Huancabamba', '3');
+INSERT INTO `afocat`.`parametros` (`codigo`, `codtab`, `descor`, `deslar`, `valent`) VALUES ('1', '04', 'Morropón', 'Morropón', '4');
+INSERT INTO `afocat`.`parametros` (`codigo`, `codtab`, `descor`, `deslar`, `valent`) VALUES ('1', '05', 'Paita', 'Paita', '5');
+INSERT INTO `afocat`.`parametros` (`codigo`, `codtab`, `descor`, `deslar`, `valent`) VALUES ('1', '06', 'Sullana', 'Sullana', '6');
+INSERT INTO `afocat`.`parametros` (`codigo`, `codtab`, `descor`, `deslar`, `valent`) VALUES ('1', '07', 'Talara', 'Talara', '7');
+INSERT INTO `afocat`.`parametros` (`codigo`, `codtab`, `descor`, `deslar`, `valent`) VALUES ('1', '08', 'Sechura', 'Sechura', '8');
+*/
